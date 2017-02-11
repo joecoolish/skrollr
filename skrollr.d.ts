0ed419d92cd099346298a4039c1687c1d49386e1
joecoolish <joe_coolish@hotmail.com>
2017-02-11 14:49:41 -0500

    Added TypeScript Definition

---
+++ skrollr.d.ts
@@ -0,0 +1,15 @@
+﻿interface SkrollrStatic {
+    get: () => any,
+    //Main entry point.
+    init: (options) => any,
+    VERSION: string,
+    menu: any
+}
+
+interface SkrollrOptions {
+    }
+
+declare module 'skrollr' {
+    const skrollr: SkrollrStatic;
+    export default skrollr;
+}
